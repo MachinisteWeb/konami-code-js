2de584f680530fc7055b7d009561726eb37809f8
Bruno Lesieur <bruno.lesieur@gmail.com>
2016-06-17 16:19:33 +0200

    Add Debug option + Possibility to disable listener + Possibility to enable listener + Possibility to change listener + Possibility to change callback + Associated options + API documentation + CDN Sharing
    Signed-off-by: Bruno Lesieur <bruno.lesieur@gmail.com>

--- src/konami-code.js
+++ src/konami-code.js
@@ -1,28 +1,67 @@
 /**
- * Fire a JavaScript Event when you enter the Up Up Bottom Bottom Left Right Left Right B A Konami Code.
+ * Create Konami Code Sequence recognition « Up Up Bottom Bottom Left Right Left Right B A » on specific HTMLElement or on global HTMLDocument.
  * @class KonamiCode
- * @version 0.1.0
+ * @version 0.2.0
  * @author {@link http://www.lesieur.name/|Bruno Lesieur}
- * @param {Object}      [options]          - Container for all options. If type of `options` is Function, is executed after Konami Code has been entered.
- * @param {Function}    [options.callback] - If `options` is not a Function, is executed after Konami Code has been entered.
- * @param {Node}        [options.sender]   - By default it's the HTMLDocument `window.document`. You can pass some HTMLElement like `<input>` (HTMLInputElement) to only listen capture of code in this element.
+ * @param {Object|Function} [options]             - Container for all options. If type of `options` is Function, it is executed after Konami Code Sequence has been recognize.
+ * @param {Function}        [options.callback]    - If `options` is not a Function, `options.callback` is executed after Konami Code Sequence has been entered.
+ * @param {Node}            [options.listener]    - By default it is the HTMLDocument `window.document`. You can pass some HTMLElement like `<input>` (HTMLInputElement) to only recognize Konami Code Sequence from this element.
+ * @param {boolean}         [options.debug]       - By default it is set to `false`. When you set this value to `true`, that allows you to see all debug message in the console.
  */
 var KonamiCode = function (options) {
-	var privates = {};
+	var publics = this,
+        privates = {};
 
-	privates.sender = (options && options.sender) || document;
-	privates.input = "";
-	privates.konamiCodeChar = "38384040373937396665";
+    /**
+     * Active the listening of Konami Code Sequence.
+     * @function enable
+     * @memberOf KonamiCode#
+     */
+    publics.enable = function () {
+        privates.listenCodeSequence();
+		privates.debug && privates.debug("Listener enabled.");
+    };
 
-	privates.afterCodeSequenceCallback =
-		(typeof options === "function" && options) ||
-		(options && typeof options.callback === "function" && options.callback) ||
-		function () { console.log("There are no action defined."); };
+    /**
+     * Unactive the listening of Konami Code Sequence.
+     * @function disable
+     * @memberOf KonamiCode#
+     */
+    publics.disable = function () {
+        privates.stopCodeSequence();
+		privates.debug && privates.debug("Listener disabled.");
+    };
+
+    /**
+     * Change the listener. The old listener will no longer work. Note: change the listener enable this instance if it is previously `disable()`.
+     * @function setListener
+     * @memberOf KonamiCode#
+     */
+    publics.setListener = function (listener) {
+    	privates.stopCodeSequence();
+		privates.listener = listener || document;
+		privates.listenCodeSequence();
+		privates.debug && privates.debug("Listener changed.", listener);
+    };
+
+     /**
+     * Change the Function executed after Konami Code Sequence has been entered.
+     * @function setCallback
+     * @memberOf KonamiCode#
+     */
+    publics.setCallback = function (callback) {
+    	privates.afterCodeSequenceCallback = (typeof callback === "function" && callback) || privates.defaultCallback;
+		privates.debug && privates.debug("Callback changed.", callback);
+    };
 
 	privates.keptLastCodeChar = function () {
 		if (privates.input.length > privates.konamiCodeChar.length) {
 			privates.input = privates.input.substr((privates.input.length - privates.konamiCodeChar.length));
 		}
+	};
+
+	privates.defaultCallback = function () {
+		privates.debug && privates.debug("Konami Code Sequence Entered. There is no action defined.");
 	};
 
 	privates.checkIfCodeCharIsValid = function () {
@@ -31,16 +70,45 @@ var KonamiCode = function (options) {
 		}
 	};
 
-	privates.listeningCodeSequence = function () {
-		privates.sender.addEventListener("keydown", function (event) {
-			privates.input += event.keyCode;
-			privates.keptLastCodeChar();
-			privates.checkIfCodeCharIsValid();
-		});
+	privates.codeSequenceEvent = function (event) {
+		privates.input += event.keyCode;
+		privates.keptLastCodeChar();
+		privates.checkIfCodeCharIsValid();
+	};
+
+    privates.stopCodeSequence = function () {
+        privates.listener.removeEventListener("keydown", privates.codeSequenceEvent);
+    };
+
+	privates.listenCodeSequence = function () {
+        privates.stopCodeSequence();
+		privates.listener.addEventListener("keydown", privates.codeSequenceEvent);
+	};
+
+	privates.checkDebugMode = function () {
+		if (options && options.debug === true) {
+			privates.debug = function (message, obj) {
+				console.log(message, obj);
+			};
+			privates.debug && privates.debug("Debug Mode On.");
+		} else {
+			privates.debug = false;
+		}
 	};
 
 	privates.init = function () {
-		privates.listeningCodeSequence();
+		privates.listener = (options && options.listener) || document;
+		privates.input = "";
+		privates.konamiCodeChar = "38384040373937396665";
+
+		privates.checkDebugMode();
+
+		privates.afterCodeSequenceCallback =
+			(typeof options === "function" && options) ||
+			(options && typeof options.callback === "function" && options.callback) ||
+			privates.defaultCallback;
+
+		privates.listenCodeSequence();
 	};
 
 	privates.init();
